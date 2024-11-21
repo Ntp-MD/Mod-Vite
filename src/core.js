45f9cc46ecf9567b35dc0f3c0041ff5bb4cbdf17
Ntpsrm <ntpsrm.md@gmail.com>
2024-11-21 10:18:14 +0700

    .

--- src/core.js
+++ src/core.js
@@ -1,10 +1,10 @@
 
-/*
+
 
 $(document).ready(async () => {
 	$('html *[id]').each(async function () {
 		for (const folder of ['components', 'views']) {
-			let path = `https://ntp-md.github.io/MOD/${folder}/${this.id}.html`;
+			let path = `https://ntp-md.github.io/Mod-Vite/${folder}/${this.id}.html`;
 			let content = await $.get(path).catch(() => null);
 			if (!content) {
 				path = `/${folder}/${this.id}.html`;
@@ -19,24 +19,3 @@ $(document).ready(async () => {
 
 
 
-$(document).ready(function () {
-	if (window.location.hostname === 'ntp-md.github.io') {
-		$('link[rel="stylesheet"]').each(function () {
-			var href = $(this).attr('href');
-			$(this).attr('href', href.replace(/^\/(.*)$/, 'https://ntp-md.github.io/MOD/$1'));
-		});
-		$('img').each(function () {
-			var src = $(this).attr('src');
-			$(this).attr('src', src.replace(/^\/(.*)$/, 'https://ntp-md.github.io/MOD/$1'));
-		});
-		$('script').each(function () {
-			var src = $(this).attr('src');
-
-			if (src && src.startsWith('/')) {
-				$(this).attr('src', src.replace(/^\/(.*)$/, 'https://ntp-md.github.io/MOD/$1'));
-			}
-		});
-	}
-});
-
-*/
