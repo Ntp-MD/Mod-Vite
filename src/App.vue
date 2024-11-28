d2db2c67f694c94148915634dea3052d2c95d943
Ntpsrm <ntpsrm.md@gmail.com>
2024-11-28 10:00:51 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+icon
 <template>
   <toggle-mode />
   <div class="dashboard-layout">
@@ -9,65 +10,16 @@
         <div class="search-box">
           <input type="search" />
           <div class="search-icon">
-            <img src="/src/assets/dashboard/search.png" alt="" width="100%" />
+            <img src="@/assets/dashboard/search.png" alt="" width="100%" />
           </div>
         </div>
         <button>Lorem, ipsum dolor.</button>
       </div>
-      <img src="/src/assets/images/mock-panel.png" alt="" width="100%" />
+      <img src="@/assets/images/mock-panel.png" alt="" width="100%" />
     </div>
   </div>
 </template>
 
 <style scoped>
-.dashboard-layout {
-  display: grid;
-  grid-template-columns: 18% auto;
-}
-
-.dashboard-content {
-  display: flex;
-  flex-direction: column;
-  padding: 2vw 5vw 5vw 2vw;
-  background: #f5f5f5;
-}
-
-.top-content {
-  display: flex;
-  justify-content: end;
-}
-
-.search-box {
-  display: flex;
-  align-items: center;
-  position: relative;
-}
-
-.search-box input {
-  width: 15vw;
-  border-radius: 30px;
-  height: 100%;
-  border: 2px solid var(--color3);
-  padding-left: 15px;
-}
-.search-box input:focus {
-  outline: none;
-}
-
-.search-icon {
-  display: grid;
-  place-items: center;
-  position: relative;
-  right: 12%;
-  background: var(--color3);
-  border-radius: 50%;
-  padding: 5px;
-  height: 20px;
-  width: 20px;
-}
-
-.search-icon img {
-  width: 80%;
-  filter: invert(1);
-}
+@import url(@/css/dashboard.css);
 </style>
--- src/main.js
+++ src/main.js
@@ -1,31 +1,28 @@
-
-import './css/main.css'
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';  // Import the router
+import "@/css/main.css";
+import { createApp } from "vue";
+import App from "@/App.vue";
+import router from "@/router"; // Import the router
 
 const app = createApp(App);
 
-app.use(router);  // Use the router in the app
-app.mount('#app');
-
-
+app.use(router); // Use the router in the app
+app.mount("#app");
 
 $(document).ready(function () {
-	if (localStorage.getItem('darkMode') === 'enabled') {
-		$('body').addClass('dark-mode');
-	}
+  if (localStorage.getItem("darkMode") === "enabled") {
+    $("body").addClass("dark-mode");
+  }
 });
 
-$(document).on('click', function (e) {
-	if ($(e.target).closest('.toggle-mode').length) {
-		$('body').toggleClass('dark-mode');
+$(document).on("click", function (e) {
+  if ($(e.target).closest(".toggle-mode").length) {
+    $("body").toggleClass("dark-mode");
 
-		// Save the mode in local storage
-		if ($('body').hasClass('dark-mode')) {
-			localStorage.setItem('darkMode', 'enabled');
-		} else {
-			localStorage.setItem('darkMode', 'disabled');
-		}
-	}
+    // Save the mode in local storage
+    if ($("body").hasClass("dark-mode")) {
+      localStorage.setItem("darkMode", "enabled");
+    } else {
+      localStorage.setItem("darkMode", "disabled");
+    }
+  }
 });
