92e5490ed5e09a97ddb052673e000d8f4d36076a
Ntp-MD <ntpsrm.md@gmail.com>
2025-02-07 18:14:34 +0700

    commit

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -39,13 +39,13 @@ export default {
 <style scoped>
 #nav-header {
   display: grid;
-  grid-template-columns: 20% auto 0 20%;
+  grid-template-columns: 20% auto 0;
   justify-items: center;
   align-items: center;
   background: #fff;
   width: 100%;
   height: 70px;
-  padding: 10px clamp(20px, 4vw, 50px);
+  padding: 10px clamp(20px, 10vw, 150px);
   box-shadow: 0 0 3px #ccc;
 }
 
@@ -59,7 +59,7 @@ export default {
 
 .header-menu {
   display: flex;
-  justify-content: center;
+  justify-content: end;
   gap: 20px;
   width: 100%;
   background: #fff;
