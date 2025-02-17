b8a94b7c39b83f9699125135a637fb2ffdbab84c
Ntp-MD <ntpsrm.md@gmail.com>
2025-02-17 15:23:11 +0700

    Updates

--- assets/Modquee-CM4cI7I2.js
+++
@@ -1,1 +0,0 @@
-import e from"./Mod-quee-CYoJOR2t.js";import{_ as n,i as _,o as c}from"./index-G81eA5CH.js";const t={};function r(s,a){const o=e;return c(),_(o)}const m=n(t,[["render",r]]);export{m as default};
--- assets/Slide-CqOthpGd.js
+++
@@ -1,1 +0,0 @@
-import t from"./Mod-Slide-ws0_dMmd.js";import{_ as e,i as n,o as r}from"./index-G81eA5CH.js";import"./3-CbRa89fe.js";import"./4-Dbmq18KG.js";import"./7-H-DOK8Er.js";const _={};function c(s,i){const o=t;return r(),n(o)}const l=e(_,[["render",c]]);export{l as default};
