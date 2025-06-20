736e5a4a1c44d9e3545216e4f315fb9f7a6a85ea
Ntp-MD <ntpsrm.md@gmail.com>
2025-06-20 17:56:14 +0700

    try remve alert

--- src/App.vue
+++ src/App.vue
@@ -21,9 +21,6 @@
 <script setup>
 import { useRoute } from "vue-router";
 import { getCurrentInstance } from "vue";
-import NavAside from "./components/NavAside.vue";
-import Header from "/src/components/Header.vue";
-import Footer from "/src/components/Footer.vue";
 import ThemeSwitch from "/src/components/ThemeSwitch.vue";
 const { appContext } = getCurrentInstance();
 const themeClass = appContext.config.globalProperties.$theme.themeClass;
